e647851cd7ecb5ebcda77a8708543cae576d020b
iaymanrezk <eng.aymanrezk@gmail.com>
2024-06-03 13:33:07 +0300

    first commit

---
+++ data/InterConnector_7.js
@@ -0,0 +1,9 @@
+var json_InterConnector_7 = {
+"type": "FeatureCollection",
+"name": "InterConnector_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Shape_Leng": 2048.20569, "Shape_Area": 68987.05354, "AssetName": "SE Interconnector" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 46.375674618308864, 24.548779544475824 ], [ 46.376376592009265, 24.548519554790868 ], [ 46.372112756594106, 24.540797852014972 ], [ 46.371540778307342, 24.541135840036517 ], [ 46.375674618308864, 24.548779544475824 ] ] ] ] } },
+{ "type": "Feature", "properties": { "Shape_Leng": 4041.58867, "Shape_Area": 273266.55083000002, "AssetName": "NE Interconnector" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 46.262497749521728, 24.647266981375434 ], [ 46.280996032591453, 24.647494473532294 ], [ 46.281022032128064, 24.646272520528946 ], [ 46.262478249192533, 24.645856536620805 ], [ 46.262497749521728, 24.647266981375434 ] ] ] ] } }
+]
+}
